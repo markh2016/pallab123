0dcc3cfac28f5ea0b666a07f939695585ddc4de7
Mark Harrington <markh2016@users.noreply.github.com>
2023-04-03 11:38:49 +0100

    Add files via upload
    These are the files  that need checking

---
+++ ReadData.cpp
@@ -0,0 +1,148 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <map>
+#include <stdio.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <limits.h>
+#include<fstream>
+
+
+
+/*C Function calls must be declared as extern and should  have prototype forward declararion 
+ implementation  is done not within the class implementation but after main method 
+ This is the linux work  around  the header #include <filesystem> */ 
+
+extern "C" {
+    void readfilesNames();
+} ;
+
+using namespace std ;
+
+class ReadData{
+
+public :
+    /* class vatriable  that holds file name */
+    
+     /* the public  constructor */ 
+    ReadData() ;
+    /*Detsructor */
+    ~ReadData() ; 
+
+
+    /*class methods */
+    uint countFileLines(string f ) ; // counts the number of lines in a given file
+    
+    /* setter and getter methods */
+    void setFileName(void) ; //sets the private instance variable filename from stdin 
+    string getFileName(void) ; // gets instance private  variable  filename 
+
+
+   
+    
+ private :   
+    
+    string _filename ;
+    uint _numfilelines ; 
+
+} ;  // end Cpp template definitions 
+
+
+/* class  implimentations   for methods start here  */
+
+ReadData::ReadData(){
+ cout << "Constructor called " << endl <<  "List of file names " << endl ;
+ readfilesNames() ;
+} ;
+
+
+// destructor 
+
+ReadData::~ReadData(){
+ cout <<  "Destructor called " << endl ; 
+
+};
+
+/* gets the file name that we want to process */
+
+void ReadData::setFileName(void)
+{
+  
+   cout<<"Please enter name of  file to process : "  ; 
+
+   cin >> _filename ; // read from std in until eol 
+   cout << endl << "filename entered was : " << _filename  << endl;
+}
+
+string ReadData::getFileName(void)
+{
+   return this->_filename;
+}
+
+/* counts the number of lines in file to be processed  */
+uint ReadData::countFileLines(string f)
+{
+     string line;  
+     uint count = 0 ; // make sure this is set to zero  using  the this pointer to instannce variable 
+
+     
+         ifstream file(f); // create input file stream object 
+
+         while (getline(file, line)){
+          count ++;
+        
+         }
+
+           cout << "Numbers of lines in the file : " <<  count << endl << endl ;
+
+
+       
+
+   return count ;
+};
+
+int main(int argc, char *argv[]){
+
+ cout<< "Program to validate data" << endl ;
+ // create instance of class 
+ ReadData m_data ;
+ // call method for reading file 
+ m_data.setFileName() ;
+
+ // count number of lines in that file 
+
+ m_data.countFileLines(m_data.getFileName()) ; 
+
+
+ return 0 ;
+}
+
+/* definition of  external c functions for  reading  current
+ directory and filnames within that directory  work around header <filesystem> on linux */
+
+
+void readfilesNames(){
+
+char cwd[PATH_MAX];
+
+DIR *d;
+    struct dirent *dir;
+    if (getcwd(cwd, sizeof(cwd)) != NULL) {
+      printf("Current working dir: %s\n\n", cwd);
+   } else {
+       perror("getcwd() error");
+   }
+
+
+    d = opendir(cwd);
+    if (d)
+    {
+        while ((dir = readdir(d)) != NULL)
+        {
+            printf("%s\n\n", dir->d_name);
+        }
+        closedir(d);
+    }
+
+};
